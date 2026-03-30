2e40e67aeb0fb1b642fceee58bc749b3e3635f22
memadi <memadi@nvidia.com>
2026-03-30 12:44:06 -0700

    remove eos from prompt template
    Signed-off-by: memadi <memadi@nvidia.com>

--- src/nemo_safe_synthesizer/llm/metadata.py
+++ src/nemo_safe_synthesizer/llm/metadata.py
@@ -818,9 +818,9 @@ class SmolLM3(ModelMetadata):
             autoconfig=config,
             instruction=DEFAULT_INSTRUCTION,
             prompt_config=LLMPromptConfig.from_tokenizer(
-                template="user\n {instruction} {schema} <|im_end|> \n <|im_start|>assistant\n{prefill}",
+                template="user\n {instruction} {schema} <|im_end|> \n assistant\n{prefill}",
                 add_bos_token_to_prompt=True,
-                add_eos_token_to_prompt=True,
+                add_eos_token_to_prompt=False,
                 tokenizer=tokenizer,
                 name=model_name_or_path,
                 bos_token=bos_token,
