97c3435f81a6e9a26438558bc75a0ae26804623d
Nina Xu <19981858+nina-xu@users.noreply.github.com>
2026-04-01 13:25:24 -0700

    fix
    Signed-off-by: Nina Xu <19981858+nina-xu@users.noreply.github.com>

--- src/nemo_safe_synthesizer/utils.py
+++ src/nemo_safe_synthesizer/utils.py
@@ -209,7 +209,7 @@ def grouped_train_test_split(
     # importing like this to avoid a dep for testing on the sdk side
     from .holdout import holdout as nss_holdout
 
-    return nss_holdout.grouped_train_test_split(df=df, test_size=test_size, group_by=group_by, random_state=seed)
+    return nss_holdout.grouped_train_test_split(input_df=df, test_size=test_size, group_by=group_by, random_state=seed)
 
 
 class DataActionsFn(Protocol):
