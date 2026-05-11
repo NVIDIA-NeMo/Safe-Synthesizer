042418704bbfef9e9aa12a2e8dd0004df75a84e7
mkornfield <mkornfield@nvidia.com>
2026-05-11 16:27:25 +0000

    address CR
    Signed-off-by: mkornfield <mkornfield@nvidia.com>

--- src/nemo_safe_synthesizer/config/training.py
+++ src/nemo_safe_synthesizer/config/training.py
@@ -256,7 +256,7 @@ class TrainingHyperparams(Parameters):
                 "are also supported."
             ),
         ),
-    ] = "sdpa"
+    ] = "FLASH_ATTN"
 
     @property
     def effective_batch_size(self) -> int:
